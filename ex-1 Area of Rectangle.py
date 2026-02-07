06ac86e5e4e158ecd959bdc39419da3646c712ec
Hashir Mohammed Hashim <hashirpunnilath@gmail.com>
2026-02-07 19:35:31 +0530

    Add files via upload

---
+++ ex-1 Area of Rectangle.py
@@ -0,0 +1,5 @@
+#Area of a Rectangle
+length = (float(input("length of the rectangle :")))
+breadth = (float(input("breadth of the rectangle :")))
+area = length * breadth
+print(f"Area of Rectangle = {area}cm²")
